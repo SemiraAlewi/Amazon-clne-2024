049c8468ea0118ae2f53a62c8e2eff55a830b203
SemiraAlewi <132842438+SemiraAlewi@users.noreply.github.com>
2024-10-26 07:46:03 +0300

    amazon final

--- amazon-clone/src/Api/axios.js
+++ amazon-clone/src/Api/axios.js
@@ -2,9 +2,9 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
 //   // localinstance of firebase function
-  baseURL: "http://127.0.0.1:5001/clone-2024-d9b68/us-central1/api",
+  // baseURL: "http://127.0.0.1:5001/clone-2024-d9b68/us-central1/api",
 //   // deployed version of amazone server on render.com
-//   // baseURL: "https://amazone-api-deploy-pnco.onrender.com",
+   baseURL: "https://amazon-api-deploy-u6fg.onrender.com",
 });
 
 export { axiosInstance };
